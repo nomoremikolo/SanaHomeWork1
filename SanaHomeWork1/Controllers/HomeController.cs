9f64d17d349d196ccdcbd9f1a1dec688fa454a4a
nomoremikolo <kn212_kmm@student.ztu.edu.ua>
2022-04-02 17:36:36 +0300

    Create simple asp dot net project

---
+++ SanaHomeWork1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaHomeWork1.Models;
+using System.Diagnostics; 
+
+namespace SanaHomeWork1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Greetings(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Greetings", user);
+            }
+            else
+            {
+                return View("Index");
+            }
+            
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SanaHomeWork1/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SanaHomeWork1.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Будьласка введіть ваше ім'я!")]
+        public string Name { get; set; }
+    }
+}
